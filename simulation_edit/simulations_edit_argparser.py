bc26ff7ae184d6abb21d92176a6317ef1159227c
noaeker <n12345e@gmail.com>
2023-08-06 16:31:36 +0300

    small fix

--- simulation_edit/simulations_edit_argparser.py
+++ simulation_edit/simulations_edit_argparser.py
@@ -3,8 +3,6 @@ def main_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str,
                         default="/Users/noa/Workspace/bootstrap_results/test2/job_0/simulations_df_fasttree.tsv")
-    parser.add_argument('--final_output_path', type=str,
-                        default="total_data.tsv")
     parser.add_argument('--work_path', type=str,
                         default='/Users/noa/Workspace/bootstrap_results/bootstrap_edit_results')
     parser.add_argument('--n_pars', type=int, default=50)
