5d6b5304abc6725721b2de0a823f10a3ef7afff9
noaeker <n12345e@gmail.com>
2023-12-26 13:50:13 +0200

    new fix

--- ML_pipeline/ML_boot_edit.py
+++ ML_pipeline/ML_boot_edit.py
@@ -244,7 +244,7 @@ def generate_data_dict_per_program(programs, folder, n_samp):
                                                      name=f'simulations_df_{program}', n_jobs=1000)
         else:
             logging.info(f"Using existing training data in {data_path} ")
-            program_data = pd.read_csv(data_path, sep='\t').sample(n=100000)
+            program_data = pd.read_csv(data_path, sep='\t')
         program_data = program_data.dropna(axis=1, how='all')
         program_data = program_data.dropna(axis=0, how='all')
         program_data= transform_data(program_data, program)
@@ -276,9 +276,9 @@ def main():
     create_dir_if_not_exists(args.working_dir)
     log_file_path = os.path.join(args.working_dir, "ML.log")
     logging.basicConfig(filename=log_file_path, level=logging.INFO)
-    main_data_dict = generate_data_dict_per_program(programs = ['raxml'], folder= args.main_data_folder, n_samp=args.n_main_samp)
+    main_data_dict = generate_data_dict_per_program(programs = ['iqtree','fasttree','raxml'], folder= args.main_data_folder, n_samp=args.n_main_samp)
     if args.use_val_data:
-        val_data_dict = generate_data_dict_per_program(programs = ['raxml'], folder= args.validation_data_folder,
+        val_data_dict = generate_data_dict_per_program(programs = ['iqtree','fasttree','raxml'], folder= args.validation_data_folder,
                                                        n_samp= args.n_val_samp)
 
     for ML_model in args.ML_model.split('_'):
